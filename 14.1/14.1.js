384521b8f0d8aabeea48ab359eab978d526ff2e3
Hila Kershenovich <hilawol@gmail.com>
2021-02-16 17:29:55 +0200

    added return value

--- 14.1/14.1.js
+++ 14.1/14.1.js
@@ -1,17 +1,17 @@
 // What is wrong with this code?
 // 1. First find the line that contains the problem. Write it down.
-// ----> Problem is in line 13,21
+// ----> Problems are in lines 13,21,24
 
 // 2. Which debug method did you use to find the bug?
 // ----> Setting breakpoints in the vsCode
 
 // 3. Explain the bug in your own words.
-// ----> In line 24 there was a comma ',' missing
-//       sum was declared as const, and later was asigned a value.
+// ----> 1.In line 24 there was a comma ',' missing
+//       2.sum was declared as const, and later was asigned a value.
+//       3.The function returns undefined. A return value was missing in the function. 
+
 
 // 4. Fix the code and submit it all.
-
-
 
 function getSum(arr1, arr2) {
   let sum = 0;
@@ -21,5 +21,6 @@ function getSum(arr1, arr2) {
   for (let i = 0; i < arr2.length; i++) {
     sum += arr2[i];
   }
+  return sum;
 }
-getSum([1, 2, 3], [5, 66, 23]);
+console.log(getSum([1, 2, 3], [5, 66, 23]));
